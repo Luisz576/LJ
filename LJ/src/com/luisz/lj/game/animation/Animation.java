2e76139469b3b292827c3a7cde4bc7791dacb446
Luisz576 <56451779+Luisz576@users.noreply.github.com>
2023-08-02 20:34:57 -0300

    animation start

---
+++ LJ/src/com/luisz/lj/game/animation/Animation.java
@@ -0,0 +1,27 @@
+package com.luisz.lj.game.animation;
+
+import java.util.*;
+
+public class Animation<T> {
+    public final String name;
+    private final List<T> frames;
+    public final List<T> getFrames(){
+        return Collections.unmodifiableList(frames);
+    }
+    public final T getFrame(int index){
+        return this.frames.get(index);
+    }
+    public final int size;
+
+    public Animation(String name, Collection<T> frames){
+        this.name = name;
+        this.size = frames.size();
+        this.frames = new ArrayList<>();
+        Iterator<T> it = frames.iterator();
+        int i = 0;
+        while(it.hasNext()){
+            this.frames.add(it.next());
+            i++;
+        }
+    }
+}
---
+++ LJ/src/com/luisz/lj/game/animation/controller/AnimationController.java
@@ -0,0 +1,11 @@
+package com.luisz.lj.game.animation.controller;
+
+import com.luisz.lj.game.animation.Animation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AnimationController<T> {
+    private final Map<String, Animation<T>> animations = new HashMap<>();
+    //TODO
+}
